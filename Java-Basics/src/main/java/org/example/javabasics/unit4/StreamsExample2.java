e866f1bf852e1ca4e6c3d30806b34a37de08d986
Deepthi Kadati <deepthikadati@gmail.com>
2023-09-27 15:04:40 -0700

    Streams

---
+++ Java-Basics/src/main/java/org/example/javabasics/unit4/StreamsExample2.java
@@ -0,0 +1,35 @@
+package org.example.javabasics.unit4;
+
+import org.example.javabasics.unit1.Person;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class StreamsExample2 {
+
+    public static void main(String[] args) {
+        //call persons data
+        List<Person> personsList = addPersonsData();
+        personsList.stream().forEach(System.out::println);
+        personsList.stream().forEach(p->p.getLastName());
+        Predicate<Person> predicate = p->p.getLastName().startsWith("C");
+        // print by condition
+        System.out.println("<----- print by last name starts with c---->");
+        personsList.stream().filter(predicate).forEach(System.out::println);
+
+        long count  = personsList.stream().filter(predicate).count();
+        System.out.println(count);
+    }
+
+    public static List<Person> addPersonsData(){
+        return Arrays.asList(
+                new Person("deepthi","Clakshmi",12),
+                new Person("pradeep","Akadati",15),
+                new Person("yogesh","Bsri",14),
+                new Person("raj","Ckumar",13)
+
+        );
+    }
+}
